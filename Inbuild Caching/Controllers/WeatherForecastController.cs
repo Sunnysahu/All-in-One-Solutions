e5b17d8e627febd75c1e2c951986cc6ba3ee0fb9
Sunny Sahu <sunnysahu8210@gmail.com>
2026-04-19 17:10:37 +0530

    Add InMemory Caching, Server Side Caching, and Client Side Caching

--- Inbuild Caching/Controllers/WeatherForecastController.cs
+++ Inbuild Caching/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Inbuild_Caching.Controllers
 {
@@ -13,22 +15,51 @@ namespace Inbuild_Caching.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly IMemoryCache _memory;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMemoryCache memory)
         {
             _logger = logger;
+            _memory = memory;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        [ResponseCache(Duration = 5)]
-        public IEnumerable<WeatherForecast> Get()
+        //[ResponseCache(Duration = 5)]
+        //[OutputCache(Duration = 60)]
+        public IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            const string cacheKey = "weatherResult";
+
+            if(!_memory.TryGetValue(cacheKey , out IEnumerable<WeatherForecast> forecast)){
+                forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+                var cacheOptions = new MemoryCacheEntryOptions()
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1),
+                    SlidingExpiration = TimeSpan.FromSeconds(30) // Resets if accessed within 30 seconds (sliding expiration)
+                };
+
+                _memory.Set(cacheKey, forecast, cacheOptions);
+                
+
+            }
+
+            var result2 = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
-            .ToArray();
+           .ToArray();
+
+            return Ok(new {forecast, result2 });
         }
     }
 }
--- Inbuild Caching/Program.cs
+++ Inbuild Caching/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddMemoryCache(); // store data in RAM
+builder.Services.AddOutputCache();
 
 var app = builder.Build();
 
@@ -16,12 +20,60 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseResponseCaching();
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+//app.UseResponseCaching();
+//app.UseOutputCache();
+
 app.Run();
+
+//```
+//builder.Services.AddMemoryCache();
+//✔️ NO attribute
+//✔️ NO middleware
+// —> for maually and caching a single or some part of datacaching
+//——————————————
+
+//Client side caching 
+
+//—>  no service needed
+//just use
+//app.UseResponseCaching();
+// in Program.cs
+
+//use annotation in controller 
+
+// [ResponseCache(Duration = 60)]  // 60 sec cache
+//————————————
+
+//server Side caching
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddOutputCache();
+
+//var app = builder.Build();
+
+//app.UseOutputCache();
+
+
+//Example with minimal APIs:
+
+//app.MapGet("/data", () =>
+//{
+//    return DateTime.Now.ToString();
+//}).CacheOutput();
+
+//Example with controller:
+
+//[OutputCache(Duration = 60)]
+//public IActionResult Get()
+//{
+//    return Ok(DateTime.Now.ToString());
+//}
+//```
+
+//They both are Time based Caching, not Change based caching
