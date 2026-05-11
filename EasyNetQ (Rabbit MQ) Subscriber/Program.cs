b296343dff48d1b2c4342ed011741bcc7c555d37
Sunny Sahu <sunnysahu8210@gmail.com>
2026-05-11 22:42:59 +0530

    Subscriber

---
+++ EasyNetQ (Rabbit MQ) Subscriber/Program.cs
@@ -0,0 +1,23 @@
+﻿using EasyNetQ;
+using Messages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel;
+
+var services = new ServiceCollection();
+
+services.AddLogging(b => b.AddConsole());
+
+services.AddEasyNetQ("host=localhost;timeout=60").UseSystemTextJson();
+
+using var provider = services.BuildServiceProvider();
+
+var bus = provider.GetRequiredService<IBus>();
+
+await bus.PubSub.SubscribeAsync<Textmessage>("Message", msg =>
+{
+    Console.WriteLine("Got Message : " + msg.Text);
+});
+
+Console.WriteLine("Listening for message. Hit <return> to quit.");
+Console.ReadLine();
