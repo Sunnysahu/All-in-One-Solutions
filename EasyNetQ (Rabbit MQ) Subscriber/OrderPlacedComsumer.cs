e2e3c1e77ae3b475715d73172f747170e87bca31
Sunny Sahu <sunnysahu8210@gmail.com>
2026-05-12 22:15:12 +0530

    Adding AutoSubscriber Feature : AutoSubscriber is a feature that automatically finds and registers message consumers/subscribers from your classes.

---
+++ EasyNetQ (Rabbit MQ) Subscriber/OrderPlacedComsumer.cs
@@ -0,0 +1,17 @@
+﻿using EasyNetQ.AutoSubscribe;
+using Messages;
+using Microsoft.Extensions.Logging;
+
+namespace EasyNetQ__Rabbit_MQ__Subscriber
+{
+    public sealed class OrderPlacedComsumer(ILogger<OrderPlacedComsumer> logger) : IConsumeAsync<OrderPlacedMessage>
+    {
+        [AutoSubscriberConsumer(SubscriptionId = "order-placed-consumer")]  
+        public Task ConsumeAsync(OrderPlacedMessage message, CancellationToken cancellationToken = default)
+        {
+            logger.LogInformation($"Received order {message.OrderId} for {message.CustomerName} - ${message.Amount}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ EasyNetQ (Rabbit MQ) Subscriber/OrderSubscriberWorker.cs
@@ -0,0 +1,23 @@
+﻿using EasyNetQ;
+using EasyNetQ.AutoSubscribe;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EasyNetQ__Rabbit_MQ__Subscriber
+{
+    public sealed class OrderSubscriberWorker(IBus bus, IServiceProvider serviceProvider, ILogger<OrderSubscriberWorker> logger) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation("Subscriber started. Registering consumers via AutoSubscriber...");
+
+            var autoSubscriber = new AutoSubscriber(bus, serviceProvider, "order-demo");
+
+            await autoSubscriber.SubscribeAsync(typeof(OrderSubscriberWorker).Assembly.GetTypes(),stoppingToken);
+
+            logger.LogInformation("AutoSubscriber ready. Waiting for messages...");
+
+            await Task.Delay(Timeout.Infinite, stoppingToken);
+        }
+    }
+}
