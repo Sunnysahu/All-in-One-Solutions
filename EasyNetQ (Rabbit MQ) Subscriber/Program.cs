c11cbb5be075e7d77126323a96d45fcad35e5906
Sunny Sahu <sunnysahu8210@gmail.com>
2026-06-04 21:20:53 +0530

    Adding AutoSubscriber Feature : AutoSubscriber is a feature that automatically finds and registers message consumers/subscribers from your classes.

--- EasyNetQ (Rabbit MQ) Subscriber/Program.cs
+++ EasyNetQ (Rabbit MQ) Subscriber/Program.cs
@@ -12,7 +12,45 @@ builder.Services
     .AddEasyNetQ("host=localhost")
     .UseSystemTextJson();
 
+
+/*
+If You Want OrderPlacedConsumer To Be Called
+
+Change:
+
+    builder.Services.AddHostedService<ManualSubscriberWorker>();
+
+to:
+
+    builder.Services.AddHostedService<OrderSubscriberWorker>();
+
+and register the consumer:
+
+    builder.Services.AddTransient<OrderPlacedComsumer>();
+
+PROCESS : 
+
+Application Start
+      |
+      v
+OrderSubscriberWorker
+      |
+      v
+AutoSubscriber
+      |
+      v
+Scan Assembly
+      |
+      v
+Find OrderPlacedComsumer
+      |
+      v
+Subscribe to OrderPlacedMessage
+ */
+
+builder.Services.AddTransient<OrderPlacedComsumer>();
 builder.Services.AddHostedService<ManualSubscriberWorker>();
+builder.Services.AddHostedService<OrderSubscriberWorker>();
 
 await builder.Build().RunAsync();
 
