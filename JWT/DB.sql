cf60c6a0cad7909716710c40f54a0f0e0e76fb7f
Sunny Sahu <sunnysahu8210@gmail.com>
2026-06-17 14:32:23 +0530

    JWT (SQL Files Added)

---
+++ JWT/DB.sql
@@ -0,0 +1,41 @@
+﻿CREATE DATABASE JwtAuthDb;
+
+GO
+
+USE JwtAuthDb;
+
+GO
+
+CREATE TABLE Users (
+    Id INT IDENTITY PRIMARY KEY,
+    Username NVARCHAR(50) UNIQUE NOT NULL,
+    Password NVARCHAR(200) NOT NULL,
+    Role NVARCHAR(50) NOT NULL
+);
+
+Go
+
+INSERT INTO Users (Username, Password, Role)
+VALUES 
+('admin','admin123','Admin'),
+('sunny','123456','User'),
+('manager','manager123','Manager')
+
+GO 
+CREATE TABLE RefreshTokens (
+    Id INT IDENTITY PRIMARY KEY,
+    UserId INT NOT NULL,
+    Token NVARCHAR(500) NOT NULL,
+    ExpiryDate DATETIME NOT NULL,
+    IsRevoked BIT DEFAULT 0,
+    CreatedDate DATETIME DEFAULT GETDATE(),
+
+    FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
+
+
+SELECT * FROM Users; SELECT * FROM RefreshTokens;
+
+
+delete  from RefreshTokens;
+GO
