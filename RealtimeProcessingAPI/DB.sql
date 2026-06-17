13828ff551d2186b942b1eb986d5fe79ffbaf66f
Sunny Sahu <sunnysahu8210@gmail.com>
2026-06-17 14:45:04 +0530

    Realtime Processing API (SQL Files Added)

---
+++ RealtimeProcessingAPI/DB.sql
@@ -0,0 +1,30 @@
+﻿CREATE DATABASE CleanStreamDB;
+GO
+
+USE CleanStreamDB;
+GO
+
+CREATE TABLE ProcessingLogs
+(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+
+    Message NVARCHAR(500) NOT NULL,
+
+    Status NVARCHAR(100) NULL,
+
+    StepNumber INT NOT NULL,
+
+    CreatedAt DATETIME2 NOT NULL
+        DEFAULT SYSUTCDATETIME()
+);
+GO
+
+
+CREATE NONCLUSTERED INDEX IX_ProcessingLogs_CreatedAt
+ON ProcessingLogs(CreatedAt DESC);
+GO
+
+SELECT *
+FROM ProcessingLogs
+ORDER BY Id;
+GO
