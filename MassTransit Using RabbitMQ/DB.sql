4e14fed3d38e145e345a005c03792cced44dccca
Sunny Sahu <sunnysahu8210@gmail.com>
2026-06-17 14:35:20 +0530

    MassTransit Using Rabbit MQ (SQL Files Added)

---
+++ MassTransit Using RabbitMQ/DB.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE OrderDb;
+GO
+
+USE OrderDb;
+GO
+
+CREATE TABLE Orders
+(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    ProductName NVARCHAR(200),
+    Quantity INT,
+    Price DECIMAL(18,2),
+    [Status] NVARCHAR(50),
+    CreatedAt DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+select * from Orders;
