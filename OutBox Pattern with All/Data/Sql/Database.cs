2a53ab00a121ac16eb4d64f6202c7de8531d24cd
Sunny Sahu <sunnysahu8210@gmail.com>
2026-06-28 00:37:02 +0530

    Sql Updated

--- OutBox Pattern with All/Data/Sql/Database.cs
+++ OutBox Pattern with All/Data/Sql/Database.cs
@@ -1,1 +1,48 @@
-﻿
+﻿USE master;
+GO
+
+ALTER DATABASE OutboxProductionDb
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE;
+GO
+
+DROP database OutboxProductionDb
+GO
+
+CREATE DATABASE OutboxProductionDb;
+GO
+
+USE OutboxProductionDb;
+GO
+
+
+CREATE TABLE Orders
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY,
+    ProductName NVARCHAR(200),
+    Quantity INT,
+    CreatedAt DATETIME2
+);
+GO
+
+CREATE TABLE OutboxMessages
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY,
+
+    EventType NVARCHAR(200),
+
+    Payload NVARCHAR(MAX),
+
+    Status INT,
+
+    RetryCount INT,
+
+    LockedBy NVARCHAR(100),
+
+    LockedAt DATETIME2,
+
+    CreatedAt DATETIME2,
+
+    ProcessedAt DATETIME2 NULL
+);
+GO
